8a72f7f736c48576ec6c460b9269284b5eaed9e0
Yousuf-Hashmi446 <155478577+Yousuf-Hashmi446@users.noreply.github.com>
2024-01-02 23:36:28 +0500

    Add files via upload

---
+++ admin.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "=========================================================================\n";
+    cout << "------------------------------- File Admin ------------------------------\n";
+    cout << "=========================================================================\n";
+
+    cout << "Welcome Admin, The Main Menu for Admin is given below:\n";
+    cout << "1- List all Files and Directories\n";
+    cout << "2- Create New Files\n";
+    cout << "3- Delete Existing Files\n";
+    cout << "4- Rename Files\n";
+    cout << "5- Edit File Content\n";
+    cout << "6- Search Files\n";
+    cout << "7- Details of Particular File\n";
+    cout << "8- View Content of File\n";
+    cout << "9- Sort File Content\n";
+    cout << "10- List only Directories(Folders)\n";
+    cout << "11- List Files of Particular Extension\n";
+    cout << "12- Count Number of Directories\n";
+    cout << "13- Count Number of Files\n";
+    cout << "14- Sort Files in a Directory\n";
+    cout << "0- Exit\n";
+
+    cout << "\nAdmin action do you want to perform?\nEnter 0-14\n";
+
+    return 0;
+}
---
+++ user.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "=========================================================================\n";
+    cout << "--------------------------------User File--------------------------------\n";
+    cout << "=========================================================================\n";
+
+    cout << "Welcome User, The Main Menu is given below:\n";
+    cout << "1- List all Files and Directories\n";
+    cout << "2- Create New Files\n";
+    cout << "3- Edit File Content\n";
+    cout << "4- View Content of File\n";
+    cout << "5- List only Directories(Folders)\n";
+    cout << "6- List Files of Particular Extension\n";
+    cout << "0- Exit\n";
+
+    cout << "\nUser what actions do you want to perform?\nEnter 0-6\n";
+
+    return 0;
+}
